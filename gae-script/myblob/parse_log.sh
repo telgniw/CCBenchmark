fe7f6e5a962fc700519ae317463573fc2c7605da
Yi Huang (Celia) <b97902044@ntu.edu.tw>
2011-04-21 15:14:44 +0800

    ADD scripts

---
+++ gae-script/myblob/parse_log.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+GREP="grep -ao"
+
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+    echo "usage: $0 <target> <limit> <output_file>"
+    exit
+fi
+
+TARGET=$1 LIMIT=$2 OUTPUT=$3
+
+TYPES[0]="upload"
+TYPES[1]="download"
+for TYPE in ${TYPES[*]}; do
+    TIMESTAMP="\[[-/: 0-9A-Za-z]\+\]"
+    ACTION="myblob $TYPE SUCCESS SimBlob[0-9]\+ [0-9]\+ [0-9]\+"
+    
+    TEST=`$GREP "$ACTION" $TARGET | head -n 1`
+    if [ -z "$TEST" ]; then
+        continue
+    fi
+
+    FILT0="$TIMESTAMP.*$ACTION"
+    FILT1="\($TIMESTAMP\)\|\($ACTION\)"
+    $GREP "$FILT0" $TARGET | $GREP "$FILT1" | head -n $LIMIT > $OUTPUT
+done
--- gae-script/myblob/retrieve_log.sh
+++ gae-script/myblob/retrieve_log.sh
@@ -3,7 +3,11 @@ APP_ENGINE_PATH="/usr/local/lib/appengine-java-sdk-1.4.3"
 APPCFG="$APP_ENGINE_PATH/bin/appcfg.sh"
 APP_PATH="/home/celia/Documents/Git/CCBenchmark/GAEBenchmark/web"
 
-TARGET="20110421/all.log"
-N_DAYS=1 SEVERITY=1
+if [ -z $1 ]; then
+    echo "usage: $0 <target>"
+    exit
+fi
+
+TARGET=$1 N_DAYS=1 SEVERITY=1
 
 $APPCFG --num_days=$N_DAYS --severity=$SEVERITY request_logs $APP_PATH $TARGET
